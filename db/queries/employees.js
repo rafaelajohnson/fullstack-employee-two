920a042dba0ce5096e43b03e082ece24e43dd45b
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-08-18 23:04:48 -0300

    employee.js

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,13 +1,27 @@
+import db from "#db/client";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const sql = `
+    INSERT INTO employees (name, birthday, salary)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+  const params = [name, birthday, salary];
+  const { rows } = await db.query(sql, params);
+  const employee = rows[0];
+
+  // Vitest expects birthday to be a Date for create/update
+  if (employee) employee.birthday = new Date(employee.birthday);
+  return employee;
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const { rows } = await db.query("SELECT * FROM employees");
+  return rows; // do NOT coerce birthday here (tests compare raw rows)
 }
 
 /**
@@ -15,7 +29,8 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const { rows } = await db.query("SELECT * FROM employees WHERE id = $1", [id]);
+  return rows[0]; // undefined if not found (as test expects)
 }
 
 /**
@@ -23,7 +38,17 @@ export async function getEmployee(id) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+  const sql = `
+    UPDATE employees
+       SET name = $1, birthday = $2, salary = $3
+     WHERE id = $4
+     RETURNING *;
+  `;
+  const params = [name, birthday, salary, id];
+  const { rows } = await db.query(sql, params);
+  const employee = rows[0];
+  if (employee) employee.birthday = new Date(employee.birthday);
+  return employee; // undefined if not found
 }
 
 /**
@@ -31,5 +56,9 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+  const { rows } = await db.query(
+    "DELETE FROM employees WHERE id = $1 RETURNING *",
+    [id]
+  );
+  return rows[0]; // undefined if not found
 }
