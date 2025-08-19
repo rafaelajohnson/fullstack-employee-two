822fadb361a8e10818e579c41e6c3671ed1bda68
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-08-18 23:06:08 -0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 import express from "express";
-const app = express();
-export default app;
+import employeesRouter from "#api/employees";
 
-// TODO: this file!
+const app = express();
+
+// Body parsing
+app.use(express.json());
+
+// Welcome route (exact string required by tests)
+app.get("/", (_req, res) => {
+  res.status(200).send("Welcome to the Fullstack Employees API.");
+});
+
+// Employees routes
+app.use("/employees", employeesRouter);
+
+// Minimal error handler to keep API tests happy
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+export default app;
